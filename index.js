9c198faa3e1a55eea16057bc6bffe6b3ee2f720a
Hovhannes Kocharyan <hovkocharyan@yandex.com>
2022-03-28 17:01:18 +0400

    feat: remove cors

--- index.js
+++ index.js
@@ -2,14 +2,9 @@ const app = require("express")();
 const server = require("http").createServer(app);
 const cors = require("cors");
 const { Server } = require("socket.io");
-const io = new Server(server, {
-  cors: {
-    origin: "*",
-    methods: ["GET", "POST"],
-  },
-});
+const io = new Server(server);
 
-app.use(cors());
+// app.use(cors());
 
 const PORT = process.env.PORT || 4200;
 
